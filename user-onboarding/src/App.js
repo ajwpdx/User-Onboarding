80e59879231dae5c0dc989638ade1f9bb4b9018e
Alex Williams <alexjwilliams1@gmail.com>
2020-07-22 15:17:06 -0700

    initial commit

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import Users from './Users'
+import Form from './Form'
+import './App.css';
+
+const teamList = []
+
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: '',
+}
+
+const fakeAxiosGet = () => {
+  return Promise.resolve({ status: 200, success: true, data: teamList })
+}
+const fakeAxiosPost = (url, { name, email, role }) => {
+  const newMember = { name, email, role }
+  return Promise.resolve({ status: 200, success: true, data: newMember })
+}
+
+
+function App() {
+  const [team, setTeam] = useState([])
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+
+  const updateForm = (inputName, inputValue) => {
+    const updatedFormValues = { ...formValues, [inputName]: inputValue }
+    setFormValues(updatedFormValues)
+  }
+
+  const submitForm = () => {
+    const newTeamMember = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    }
+    if (!newTeamMember.name || !newTeamMember.email || !newTeamMember.role) return
+
+    fakeAxiosPost('fakeapi.com', newTeamMember)
+      .then(res => {
+        setTeam([res.data, ...team])
+        setFormValues(initialFormValues)
+      })
+
+  }
+
+  useEffect(() => {
+    fakeAxiosGet('fakeapi.com').then(res => setTeam(res.data))
+  }, [])
+
+
+
+  //JSX
+  return (
+    <div className="App">
+      <Form
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}
+        {...team.map(member => {
+          return (
+            <Users key={member.email} details={member} />
+          )
+        })
+        }
+      />
+    </div>
+  );
+}
+
+
+
+export default App;
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+
+export default function Form(props) {
+    
+    const { values, update, submit } = props
+
+    const onChange = evt => { 
+    const name = evt.target.name
+    const value = evt.target.value
+    update(name, value)
+  }
+
+    const onSubmit = evt => {
+       evt.preventDefault()
+       submit()
+      }
+
+    return(
+        <form onSubmit={onSubmit}>
+            <h2>Add a Team Member</h2>
+            <div className='form-inputs'>
+            <label htmlFor='nameInput'>Name:&nbsp;
+                <input
+                id = 'nameInput'
+                name = 'name'
+                type = 'text'
+                placeholder= 'Enter name'
+                value='{values.name}'
+                onChange='{onChange}'
+                />
+            </label>
+            <label htmlFor='emailInput'>Email:&nbsp;
+                <input
+                id = 'emailInput'
+                name = 'email'
+                type = 'text'
+                placeholder= 'Enter email'
+                value='{values.email}'
+                onChange='{onChange}'
+                />
+            </label>
+            <label>Role:&nbsp;
+            <select name='role' value='{values.role}' onChange='{onChange}'>
+              <option value=''>Select a role</option>
+              <option value='Skateboarder'>Skateboarder</option>
+              <option value='Surfer'>Surfer</option>
+              <option value='Snowboarder'>Snowboarder</option>
+            </select>
+        </label>
+            </div>
+            <button>Submit</button>    
+
+        </form>
+    )
+    
+}
