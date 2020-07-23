b67ffff435210656052c9ac1a2c4a3f7cc272d02
Alex Williams <alexjwilliams1@gmail.com>
2020-07-23 12:34:03 -0700

    added cypress and form.spec file for e2e testing

---
+++ cypress/integration/form.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+describe('Inputs and submit button', () => {
+    it('can navigate to the site;', () => {
+        //this is one test
+        cy.visit('http://localhost:3000/')
+
+    })
+
+})
